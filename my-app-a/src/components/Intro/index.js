a68e9af9cf60f0a81ed183595f59a64e91bc80c9
avri-el <parengkuanavriel@gmail.com>
2024-11-15 03:14:06 +0800

    modify Intro

--- my-app-a/src/components/Intro/index.js
+++ my-app-a/src/components/Intro/index.js
@@ -1,7 +1,21 @@
 const Intro = () => {
   return (
-    <div className="intro">
-      <h1>INTRODUCTION</h1>
+    <div className="container2">
+      <div className="intro">
+        <h1>INTRODUCTION</h1>
+      </div>
+      <div className="content">
+        <p>
+          I'm Avriel Tirza Priskila Parengkuan, an 18-year-old student at
+          Universitas Klabat (Unklab). With a strong interest in design, I am
+          constantly exploring new ways to approach challenges creatively. This
+          portfolio highlights my journey and the projects I've had the
+          opportunity to work on so far.
+        </p>
+        <div className="images2">
+          <img src="el.jpg" alt="Image 2" />
+        </div>
+      </div>
     </div>
   );
 };
