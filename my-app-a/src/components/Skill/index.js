6e7bce508066a10ab50f4187cc25ba94edc84eb6
avri-el <parengkuanavriel@gmail.com>
2024-11-15 03:14:32 +0800

    modify skill

--- my-app-a/src/components/Skill/index.js
+++ my-app-a/src/components/Skill/index.js
@@ -1,19 +1,24 @@
-const Skill = () => {
+const Skills = () => {
   return (
-    <div className="skill">
-      <h1>SKILL AND EXPERIENCE</h1>
-      <div className="content">
-        <p>
-          I’m skilled in time management, communication, and adaptability...
-        </p>
-        <div className="images3">
-          <img src="uv2.jpg" alt="image3" />
+    <div className="container3">
+      <div className="skill">
+        <h1>SKILL AND EXPERIENCE</h1>
+        <div className="content">
+          <p>
+            I’m skilled in time management, communication, and adaptability,
+            which help me work well in any environment. I have experience in
+            English, graphic design, photography, and web design, creating
+            visually engaging and functional websites.
+          </p>
+          <div className="images3">
+            <img src="uv2.jpg" alt="Image 3" />
+          </div>
+          <br />
+          <p>
+            In addition to my technical and creative skills, I have valuable
+            experience in organizational settings.
+          </p>
         </div>
-        <br />
-        <p>
-          In addition to my technical and creative skills, I have valuable
-          experience...
-        </p>
       </div>
     </div>
   );
