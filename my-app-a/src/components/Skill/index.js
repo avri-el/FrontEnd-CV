a4c8525f6c1c5e0302aff74013ae4b5020755483
avri-el <parengkuanavriel@gmail.com>
2024-11-15 02:43:36 +0800

    add Skill

---
+++ my-app-a/src/components/Skill/index.js
@@ -0,0 +1,21 @@
+const Skill = () => {
+  return (
+    <div className="skill">
+      <h1>SKILL AND EXPERIENCE</h1>
+      <div className="content">
+        <p>
+          I’m skilled in time management, communication, and adaptability...
+        </p>
+        <div className="images3">
+          <img src="uv2.jpg" alt="image3" />
+        </div>
+        <br />
+        <p>
+          In addition to my technical and creative skills, I have valuable
+          experience...
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Skill;
