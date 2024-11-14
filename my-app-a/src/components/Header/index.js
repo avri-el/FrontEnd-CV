a04d9382eeabe00215552c7b1f361f1cd430736d
avri-el <parengkuanavriel@gmail.com>
2024-11-15 02:40:42 +0800

    import export header

---
+++ my-app-a/src/components/Header/index.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <span>PORTFOLIO</span>
+      <span style={{ float: "right" }}>2024 &nbsp; | &nbsp; 14</span>
+    </div>
+  );
+};
+
+export default Header;
