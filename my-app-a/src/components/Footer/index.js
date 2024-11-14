9d37116dec0bb73b1229165b02eb397df4362447
avri-el <parengkuanavriel@gmail.com>
2024-11-15 02:45:29 +0800

    add footer

---
+++ my-app-a/src/components/Footer/index.js
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="social-links">
+        <a
+          href="https://www.linkedin.com/in/avriel-parengkuan-09257b317?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://instagram.com/el.avie"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>
+      </div>
+      <a href="mailto:parengkuanavriel@gmail.com">Contact Me via Email</a>
+      <p>© 2024 Avriel. All rights reserved.</p>
+    </footer>
+  );
+};
+export default Footer;
