caff4cbc3a0939da369b1cf5adc9903b5023a5c3
avri-el <parengkuanavriel@gmail.com>
2024-11-15 03:37:15 +0800

    config firebase again

---
+++ my-app-a/src/components/config/Firebase/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6VlNygo1K8aVtGGJMGRf_13ODMigvSUY",
+  authDomain: "frontend-cv.firebaseapp.com",
+  databaseURL: "https://frontend-cv-default-rtdb.firebaseio.com",
+  projectId: "frontend-cv",
+  storageBucket: "frontend-cv.firebasestorage.app",
+  messagingSenderId: "153656658579",
+  appId: "1:153656658579:web:405d8f6b51af5cf1eddb66",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
