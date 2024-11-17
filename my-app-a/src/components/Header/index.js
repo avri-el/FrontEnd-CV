7868f4835bafce20d60a0ae7311b279ceab4a0fa
avri-el <parengkuanavriel@gmail.com>
2024-11-18 01:40:25 +0800

    add firebase header

--- my-app-a/src/components/Header/index.js
+++ my-app-a/src/components/Header/index.js
@@ -1,4 +1,18 @@
+import { ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+import { database } from "../../config/Firebase/firebase";
+
 const Header = () => {
+  const [header, setHeader] = useState({});
+
+  useEffect(() => {
+    const headerRef = ref(database, "header/"); // Gunakan instance database yang sudah diinisialisasi
+    onValue(headerRef, (snapshot) => {
+      const data = snapshot.val();
+      setHeader(data);
+    });
+  }, []);
+
   return (
     <div className="container1">
       <div className="header">
@@ -6,22 +20,15 @@ const Header = () => {
         <span style={{ float: "right" }}>2024 &nbsp; | &nbsp; 14</span>
       </div>
       <div className="title">
-        Welcome <br />
-        <h6>to Avriel's portfolio.</h6>
+        {header.title1} <br />
+        <h6>{header.title2}</h6>
         <div className="images">
           <img src="/img/ell.jpg" alt="Image 1" />
         </div>
       </div>
       <div className="content">
         <div className="text">
-          <p>
-            I am a web designer and graphic designer, with a passion for
-            creating high-quality work. Here, you can explore a collection of
-            projects I've worked on. I strive to deliver the best in every
-            project I undertake, with a focus on detail, creativity, and
-            innovative solutions. Thank you for visiting, and I hope you find
-            something that inspires you!
-          </p>
+          <p>{header.text}</p>
         </div>
       </div>
     </div>
