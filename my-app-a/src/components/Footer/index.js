d53562e478a13f4be600056f1102daff86108fbd
avri-el <parengkuanavriel@gmail.com>
2024-11-18 02:20:17 +0800

    add firebase to footer

--- my-app-a/src/components/Footer/index.js
+++ my-app-a/src/components/Footer/index.js
@@ -3,31 +3,23 @@ import { useEffect, useState } from "react";
 import { database } from "../../config/Firebase/firebase";
 
 const Footer = () => {
-  const [project, setProject] = useState({});
+  const [links, setLinks] = useState({});
 
   useEffect(() => {
-    const projectRef = ref(database, "project/"); // Gunakan instance database yang sudah diinisialisasi
-    onValue(projectRef, (snapshot) => {
+    const linksRef = ref(database, "links/"); // Gunakan instance database yang sudah diinisialisasi
+    onValue(linksRef, (snapshot) => {
       const data = snapshot.val();
-      setProject(data);
+      setLinks(data);
     });
   }, []);
 
   return (
     <footer>
       <div className="social-links">
-        <a
-          href="https://www.linkedin.com/in/avriel-parengkuan-09257b317?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href={links.linkedin} target="_blank" rel="noreferrer">
           LinkedIn
         </a>
-        <a
-          href="https://instagram.com/el.avie"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href={links.ig} target="_blank" rel="noreferrer">
           Instagram
         </a>
       </div>
