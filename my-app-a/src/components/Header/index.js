8da48792a1845be8ad6500f3c70b67dbfa8e343c
avri-el <parengkuanavriel@gmail.com>
2024-11-19 20:44:15 +0800

    modify react from header

--- my-app-a/src/components/Header/index.js
+++ my-app-a/src/components/Header/index.js
@@ -7,37 +7,57 @@ import { Fade } from "react-awesome-reveal";
 const Header = () => {
   const [header, setHeader] = useState({});
   const [images, setImages] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const headerRef = ref(database, "header/"); // Gunakan instance database yang sudah diinisialisasi
-    onValue(headerRef, (snapshot) => {
-      const data = snapshot.val();
-      setHeader(data);
-    });
-    const imagesRef = ref(database, "images/"); // Gunakan instance database yang sudah diinisialisasi
-    onValue(imagesRef, (snapshot) => {
-      const data = snapshot.val();
-      setImages(data);
-    });
+    const fetchData = async () => {
+      try {
+        const headerRef = ref(database, "header/");
+        onValue(headerRef, (snapshot) => {
+          const data = snapshot.val();
+          if (data) setHeader(data);
+        });
+
+        const imagesRef = ref(database, "images/");
+        onValue(imagesRef, (snapshot) => {
+          const data = snapshot.val();
+          if (data) setImages(data);
+        });
+      } catch (error) {
+        console.error("Error fetching data from Firebase:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Fade>
       <div className="container1">
         <div className="header">
           <span>PORTFOLIO</span>
-          <span style={{ float: "right" }}>2024 &nbsp; | &nbsp; 14 Nov</span>
+          <span className="header-date">2024 &nbsp; | &nbsp; 14 Nov</span>
         </div>
+
         <div className="title">
-          {header.title1} <br />
+          <h1>{header.title1}</h1>
           <h6>{header.title2}</h6>
-          <div className="images">
-            <img
-              src={`data:image/jpeg;base64, ${images.header}`}
-              alt="Image 1"
-            />
-          </div>
+          {images.header && (
+            <div className="images">
+              <img
+                src={`data:image/jpeg;base64, ${images.header}`}
+                alt={header.title1}
+              />
+            </div>
+          )}
         </div>
+
         <div className="content">
           <div className="text">
             <p>{header.text}</p>
